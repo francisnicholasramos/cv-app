9ba0367eb4b42ec333e5167539853e758fde8095
Francis Nicholas Ramos <francis.ramos@aiqueinnovation.com>
2025-04-04 02:10:22 +0800

    Fixed input props

--- my-app/app/components/BasicInfo.tsx
+++ my-app/app/components/BasicInfo.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import BasicInput from "./BasicInput";
-import "../../assets/styles/basicInfo.scss";
 
 const BasicInfo = () => {
+  const [fullname, setFullName] = useState("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFullName(e.target.value);
+  };
+
   return (
-    <div>
-      <div className="max-w-[350] ">
-        <BasicInput id="full-name" labelText="Full Name" type="text" />
-        <BasicInput id="email" labelText="Email" type="email" />
-        <BasicInput id="github" labelText="Github" type="text" />
-        <BasicInput id="linkedin" labelText="Phone number" type="number" />
+    <div className="h-screen p-10 flex justify-center min-w-3xl bg-[#d9d9d9]">
+      <div className="flex h-full gap-5">
+        <div className="flex flex-col w-[400px]">
+          <BasicInput
+            id="full-name"
+            labelText="Full Name"
+            type="text"
+            onChange={handleInput}
+            className="mt-0"
+          />
+          <BasicInput id="email" labelText="Email" type="email" />
+          <BasicInput id="github" labelText="Github" type="text" />
+          <BasicInput id="linkedin" labelText="Phone number" type="number" />
+        </div>
+        
+        <div className="w-[700px] max-w-[700px] bg-[#f5f5f5]">
+          <div className="w-full flex justify-center items-center bg-[#0e374f] h-[5rem]">
+            <h2 className="text-[#e6e6e6] text-3xl font-bold">{fullname}</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- my-app/app/components/BasicInput.tsx
+++ my-app/app/components/BasicInput.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Input } from '@heroui/input'
+import React from "react";
+import { Input } from "@heroui/input";
 
 interface Info {
-    id: string
-    type: string
-    labelText: string
+  id: string;
+  type: string;
+  labelText: string;
+  className?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-const BasicInput = ({ id, type, labelText,// onChange, value
+const BasicInput = ({
+  id,
+  type,
+  labelText,
+  className,
+  onChange, // value
 }: Info) => {
-    return (
-        <div>
-            <Input
-                className="mt-[10px]" 
-                id={id}
-                type={type}
-                label={labelText}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="w-full">
+      <Input
+        className={`mt-3 ${className}`}
+        id={id}
+        type={type}
+        label={labelText}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
 
-export default BasicInput
+export default BasicInput;
