035c1d1ed4b67d102a9df46e302e5d69a1295818
Francis Nicholas Ramos (Intern) <pfrancisnicholasramos@gmail.com>
2025-04-04 14:40:28 +0800

    Created reusable folder

---
+++ my-app/app/reusable/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div className='border-b-2 border-[#000000] mx-10'>
+      <h2>test</h2>
+    </div>
+  )
+}
+
+export default Section
---
+++ my-app/app/reusable/ReusableInput.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Input } from "@heroui/input";
+
+interface Info {
+  id: string;
+  type: string;
+  labelText: string;
+  className?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const ReusableInput = ({
+  id,
+  type,
+  labelText,
+  className,
+  onChange, // value
+}: Info) => {
+  return (
+    <div className="w-full">
+      <Input
+        className={`mt-3 ${className}`}
+        id={id}
+        type={type}
+        label={labelText}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default ReusableInput;
