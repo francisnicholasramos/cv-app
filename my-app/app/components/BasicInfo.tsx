06613f577ff48d28e7304327b7e3ab2d74ede2d7
Francis Nicholas Ramos (Intern) <pfrancisnicholasramos@gmail.com>
2025-04-04 14:50:29 +0800

    Added useState for every inputs

--- my-app/app/components/BasicInfo.tsx
+++ my-app/app/components/BasicInfo.tsx
@@ -1,33 +1,70 @@
 "use client";
 import React, { useState } from "react";
-import BasicInput from "./BasicInput";
+import ReusableInput from "@/app/reusable/ReusableInput";
+import Header from "@/app/reusable/Header";
 
 const BasicInfo = () => {
   const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [github, setGithub] = useState("");
+  const [phone, setPhone] = useState("");
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFullName(e.target.value);
+  };
+
+  const handleInputEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleInputGithub = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGithub(e.target.value);
+  };
+
+  const handleInputPhone = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
   };
 
   return (
     <div className="h-screen p-10 flex justify-center min-w-3xl bg-[#d9d9d9]">
       <div className="flex h-full gap-5">
         <div className="flex flex-col w-[400px]">
-          <BasicInput
+          <ReusableInput
             id="full-name"
             labelText="Full Name"
             type="text"
-            onChange={handleInput}
+            onChange={handleInputName}
             className="mt-0"
           />
-          <BasicInput id="email" labelText="Email" type="email" />
-          <BasicInput id="github" labelText="Github" type="text" />
-          <BasicInput id="linkedin" labelText="Phone number" type="number" />
+          <ReusableInput
+            id="email"
+            labelText="Email"
+            type="email"
+            onChange={handleInputEmail}
+          />
+          <ReusableInput
+            id="github"
+            labelText="Github"
+            type="text"
+            onChange={handleInputGithub}
+          />
+          <ReusableInput
+            id="linkedin"
+            labelText="Phone number"
+            type="number"
+            onChange={handleInputPhone}
+          />
         </div>
-        
+
         <div className="w-[700px] max-w-[700px] bg-[#f5f5f5]">
-          <div className="w-full flex justify-center items-center bg-[#0e374f] h-[5rem]">
-            <h2 className="text-[#e6e6e6] text-3xl font-bold">{fullname}</h2>
+          <Header />
+          <div className="w-full flex flex-col justify-center items-center h-[5rem]">
+            <h2 className="text-[#333333] text-3xl font-medium">{fullname}</h2>
+            <div>
+              <p>{email}</p>
+              <p>{github}</p>
+              <p>{phone}</p>
+            </div>
           </div>
         </div>
       </div>
